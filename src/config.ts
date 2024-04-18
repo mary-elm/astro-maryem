fe34a321559375b1402f162b77be5aca98c2ca62
maryem lamhamdi <maryemlemhamdi@gmail.com>
2024-04-18 03:11:42 +0200

    Update name on website.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Home | Enrique Shunnar";
+export const SITE_TITLE = "Home | Maryem Lamhamdi";
 export const SITE_DESCRIPTION =
   "I'm a self-taught Software Engineer based in Panama🇵🇦, and Expert in the Javascript React Ecosystem with Typescript. Check out my Blog, CV, Project Section, Store and RSS Feed.";
